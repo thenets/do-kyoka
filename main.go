37b8845736586beb0589e168d61ae726300e6442
Luiz Costa <phelip@thenets.org>
2020-08-14 08:51:55 -0300

    Add scheduler and log lib

--- main.go
+++ main.go
@@ -6,38 +6,58 @@ import (
 	"os"
 
 	"github.com/digitalocean/godo"
+	"github.com/jasonlvhit/gocron"
+	log "github.com/sirupsen/logrus"
 	"github.com/thenets/do-kyoka/helper"
 )
 
-func main() {
+func updateFirewall() {
 	// Read env vars
 	firewallName := os.Getenv("FIREWALL_NAME")
 	if firewallName == "" {
-		panic("[ERROR] 'FIREWALL_NAME' not set in config file!")
+		log.Panic("'FIREWALL_NAME' not set in config file!")
 	}
 	tagName := os.Getenv("FIREWALL_TAG")
 	if tagName == "" {
-		panic("[ERROR] 'FIREWALL_TAG' not set in config file!")
+		log.Panic("'FIREWALL_TAG' not set in config file!")
 	}
 	apiToken := os.Getenv("DO_API_TOKEN")
 	if tagName == "" {
-		panic("[ERROR] 'DO_API_TOKEN' not set in config file!")
+		log.Panic("'DO_API_TOKEN' not set in config file!")
 	}
-	
-	// DEBUG
-	fmt.Println("firewallName", firewallName)
-	fmt.Println("tagName", tagName)
-	fmt.Println("apiToken", apiToken)
-	return
 
 	// Firewall: allow my current IP
 	client := godo.NewFromToken(apiToken)
 	ctx := context.TODO()
 	firewall, err := helper.FirewallAllowMyCurrentIp(ctx, client, firewallName, tagName)
 	if err != nil {
-		panic(nil)
+		// Output Firewall information
+		log.WithFields(log.Fields{
+			"ID":            firewall.ID,
+			// "Name":          firewall.Name,
+			// "Status":        firewall.Status,
+			"InboundRules":  firewall.InboundRules,
+			"OutboundRules": firewall.OutboundRules,
+		}).Error(fmt.Sprintf("firewall '%s' update %s", firewall.Name, firewall.Status))
 	}
 
 	// Output Firewall information
-	fmt.Println(firewall)
+	log.WithFields(log.Fields{
+		"ID":            firewall.ID,
+		// "Name":          firewall.Name,
+		// "Status":        firewall.Status,
+		// "InboundRules":  firewall.InboundRules,
+		// "OutboundRules": firewall.OutboundRules,
+	}).Info(fmt.Sprintf("firewall '%s' update %s", firewall.Name, firewall.Status))
+}
+
+func main() {
+	// Run updateFirewall once
+	updateFirewall()
+
+	// Add updateFirewall to scheduler
+	gocron.Every(30).Minutes().Do(updateFirewall)
+
+	// Start scheduler
+	<-gocron.Start()
 }
